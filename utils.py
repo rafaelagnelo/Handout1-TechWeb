1151c23f09e065ae5f7b35c4144bf6e152f67772
Rafael Agnelo <rafaelca13@al.insper.edu.br>
2024-02-14 21:28:01 -0300

    finalizando

--- utils.py
+++ utils.py
@@ -28,3 +28,32 @@ def load_template(filename):
         content = file.read()
     return content
 
+def build_response(body='', code=200, reason='OK', headers=''):
+    if body:
+        response = "HTTP/1.1 {} {}\n\n{}".format(code, reason, body)
+    elif headers:
+        response = "HTTP/1.1 {} {}\n{}\n\n".format(code, reason, headers)
+    else:
+        response = "HTTP/1.1 {} {}\n\n".format(code, reason)
+    return response.encode()
+
+def add_annotation_to_notes(titulo, detalhes):
+  """
+  Adiciona uma anotação ao arquivo notes.json.
+
+  Args:
+    titulo: O título da anotação.   
+    detalhes: A descrição da anotação.
+
+  Returns:
+    None.
+  """
+
+  with open("data/notes.json", "r") as f:
+    notes = json.load(f)
+
+  
+  notes.append({"titulo": titulo, "detalhes": detalhes})
+
+  with open("data/notes.json", "w") as f:
+    json.dump(notes, f, indent=2)
--- views.py
+++ views.py
@@ -1,8 +1,35 @@
-from utils import load_data, load_template
+from utils import load_data, load_template, add_annotation_to_notes, build_response
+from urllib.parse import unquote_plus
 
-def index():
+
+def index(request):
     # Cria uma lista de <li>'s para cada anotação
     # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+
+        for chave_valor in corpo.split('&'):
+            chave, valor = chave_valor.split('=')
+            params[chave] = unquote_plus(valor)
+        titulo = params.get('titulo', '')
+        detalhes = params.get('detalhes', '')
+        add_annotation_to_notes(titulo, detalhes)  # Adiciona a nova anotação ao arquivo notes.json
+        return build_response(code=303, reason='See Other', headers='Location: /')
+
+
+            
+    
+    
     note_template = load_template('components/note.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
@@ -10,4 +37,4 @@ def index():
     ]
     notes = '\n'.join(notes_li)
 
-    return load_template('index.html').format(notes=notes).encode()
+    return build_response(body=load_template('index.html').format(notes=notes))
