009ac0a4a6456b0db0d3f4742b47073d6525d860
Rafael Agnelo <rafaelca13@al.insper.edu.br>
2024-02-27 10:00:30 -0300

    arrumando

--- servidor.py
+++ servidor.py
@@ -12,7 +12,7 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 server_socket.listen()
 
-print(f'Servidor escutando em (ctrl+click): http://{SERVER_HOST}:{SERVER_PORT}')
+print(f'Servidor escutando em (ctrl+click): http://127.0.0.1:{SERVER_PORT}')
 
 while True:
     client_connection, client_address = server_socket.accept()
